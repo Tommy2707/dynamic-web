02c768989834c0ff3fd4bbaf239747a193c8a2c1
Tommy2707 <82205595+Tommy2707@users.noreply.github.com>
2021-04-10 18:56:01 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@ $password = "78a7dd7415e3cb879d5d99e6a1de69acb74314083261b47dc56afd38cabddb06";
 $dbname = "Menu";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new postgresqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
