f5cbe85b1da0d691114309c3d4a43bdf32eb9a85
Tommy2707 <82205595+Tommy2707@users.noreply.github.com>
2021-04-10 19:15:25 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 
 <?php
-/*$servername = "postgres";
+/*$servername = "postgresql";
 $username = "yimmyalkizdwan";
 $password = "78a7dd7415e3cb879d5d99e6a1de69acb74314083261b47dc56afd38cabddb06";
 $dbname = "Menu";
