e4c902198378c88be4968b697ac42e38e16b140a
Tommy2707 <82205595+Tommy2707@users.noreply.github.com>
2021-04-10 18:51:37 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,8 +1,8 @@
 <?php
-/*$servername = "localhost";
-$username = "root";
-$password = "mysql";
-$dbname = "menu";
+/*$servername = "postgres";
+$username = "yimmyalkizdwan";
+$password = "78a7dd7415e3cb879d5d99e6a1de69acb74314083261b47dc56afd38cabddb06";
+$dbname = "Menu";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
