bd83f86e4be7164688de7fd129eb0ae9c67f1bb0
Tommy2707 <82205595+Tommy2707@users.noreply.github.com>
2021-04-14 20:30:32 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,18 @@
 
 <?php
-/*$servername = "postgres://yimmyalkizdwan:78a7dd7415e3cb879d5d99e6a1de69acb74314083261b47dc56afd38cabddb06@ec2-52-45-73-150.compute-1.amazonaws.com:5432/d64k2ekkin6c2v";
-$username = "yimmyalkizdwan";
-$password = "78a7dd7415e3cb879d5d99e6a1de69acb74314083261b47dc56afd38cabddb06";
-$dbname = "Menu";
-
-// Create connection
-$conn = new postgresqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+// connect to a database 
+$dbConn = pg_connect("host=ec2-52-45-73-150.compute-1.amazonaws.com port=5432 dbname=d64k2ekkin6c2v user=yimmyalkizdwan password=78a7dd7415e3cb879d5d99e6a1de69acb74314083261b47dc56afd38cabddb06");
+if (!$dbConn) {
+    echo "An error occurred.\n";
+    exit;
 }
-?>
-*/
+// Query data
+$result = pg_query($dbConn, 'SELECT * FROM Menu');
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+// Show value
+while ($row = pg_fetch_assoc($result)) {
+    var_dump($row);
+}
