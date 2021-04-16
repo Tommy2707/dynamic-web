5db1ecadc83139cc34258d4cdd892d623f5a74a9
Tommy2707 <82205595+Tommy2707@users.noreply.github.com>
2021-04-16 16:39:07 +0700

    Update create-connection.php

--- create-connection.php
+++ create-connection.php
@@ -1,13 +1,13 @@
 <?php
-$Host = "ec2-52-45-73-150.compute-1.amazonaws.com";
-$Port = "5432";
-$Database = "d64k2ekkin6c2v";
-$User = "yimmyalkizdwan";
-$Password = "78a7dd7415e3cb879d5d99e6a1de69acb74314083261b47dc56afd38cabddb06";
-//Create connection
-$dbConn = new postgresqli($Host, $Port, $Database, $User, $Password);
-//Check connection
-if ($dbConn->connect_error){
-  die("Connection failed: " . $dbConn->connect-error);
-}
+   $host        = "host = ec2-52-45-73-150.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = d64k2ekkin6c2v";
+   $credentials = "user = yimmyalkizdwan password=78a7dd7415e3cb879d5d99e6a1de69acb74314083261b47dc56afd38cabddb06";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
 ?>
