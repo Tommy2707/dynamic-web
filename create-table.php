6a0077ec53f4628b47b07f86236a920da46247f6
Tommy2707 <82205595+Tommy2707@users.noreply.github.com>
2021-04-16 14:00:56 +0700

    Update create-table.php

--- create-table.php
+++ create-table.php
@@ -1,11 +1,11 @@
 <?php
-$host = "ec2-52-45-73-150.compute-1.amazonaws.com";
-$port = "5432";
-$dbname = "d64k2ekkin6c2v";
-$user = "yimmyalkizdwan";
-$password = "78a7dd7415e3cb879d5d99e6a1de69acb74314083261b47dc56afd38cabddb06";
+$Host = "ec2-52-45-73-150.compute-1.amazonaws.com";
+$Port = "5432";
+$Database = "d64k2ekkin6c2v";
+$User = "yimmyalkizdwan";
+$Password = "78a7dd7415e3cb879d5d99e6a1de69acb74314083261b47dc56afd38cabddb06";
 //Create connection
-$dbConn = new postgresqli($host, $port, $dbname, $user, $password);
+$dbConn = new postgresqli($Host, $Port, $Database, $User, $Password);
 //Check connection
 if ($dbConn->connect_error){
   die("Connection failed: " . $dbConn->connect-error);
